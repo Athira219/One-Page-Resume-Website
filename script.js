13c16677902c901aff3f22c5dac1a445bc8142dc
Athira Somaraj <athirasomaraj070gmail.com>
2022-09-14 14:54:40 +0530

    one page resume website created

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".toggle");
+const show = document.querySelector(".show");
+
+menuToggle.addEventListener("click", function(){
+  menuToggle.classList.toggle("active");
+  show.classList.toggle("active");
+});
